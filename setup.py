bdee37eb470b6efc709d25c71131ffac96c142ca
Mike Boers <github@mikeboers.com>
2010-09-17 12:28:54 -0400

    Add a setup file.

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from distutils.core import setup
+
+setup(
+    name='AutoCache',
+    version='0.1a',
+    description='Simple cache and memoizing module.',
+    url='http://github.com/mikeboers/AutoCache',
+    
+    packages=['autocache'],
+    
+    author='Mike Boers',
+    author_email='autcache@mikeboers.com',
+    license='New BSD License',
+)

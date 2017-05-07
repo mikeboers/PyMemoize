1aa689f509d5837efeccc7e888ce1231f3212ccb
Mike Boers <github@mikeboers.com>
2017-05-07 19:32:22 -0400

    Bump to v1.0.2.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='PyMemoize',
-    version='1.0.1',
+    version='1.0.2',
     description='Simple memoizing module.',
     url='http://github.com/mikeboers/PyMemoize',
     
